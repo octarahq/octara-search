9eab90b73da50e787d66562127c536229eb24367
lotus64yt <lotus64santon@gmail.com>
2026-04-11 03:14:03 +0200

    fix config file type

--- frontend/src/config/auth-config.ts
+++ frontend/src/config/auth-config.ts
@@ -1,6 +1,16 @@
-import config from "../../../config.json";
+import configData from "../../../config.json";
+
+interface Config {
+  databaseUrl?: string;
+  oauth?: {
+    clientId: string;
+    clientSecret: string;
+  };
+}
+
+const config = configData as Config;
 
 export const AUTH_CONFIG = {
-  clientId: config.oauth.clientId,
-  clientSecret: config.oauth.clientSecret,
+  clientId: config.oauth?.clientId || "",
+  clientSecret: config.oauth?.clientSecret || "",
 };
